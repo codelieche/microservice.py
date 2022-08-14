aff99256ac5342eaab4f2ac80d86d9d9b218d9bd
codelieche <codelieche@gmail.com>
2022-08-14 09:29:28 +0800

    BaseModel add do_delete_action function

--- codelieche/src/codelieche/django/models/base.py
+++ codelieche/src/codelieche/django/models/base.py
@@ -10,7 +10,11 @@ class BaseModel(models.Model):
     基础Model类
     添加了deleted字段, 覆写了save方法
     """
+    delete_tasks = ()  # 需要执行的删除函数的任务（函数列表）
     SECRET_FIELDS = ()  # 需要加密的字段
+    delete_update_fields = ()  # 删除的时候需要自动修改的字段
+    delete_time_field_name = None  # 删除的时候自动记录时间
+    # delete_time_field_name = "time_deleted"  # 删除的时候自动记录时间
 
     # 字段：删除、添加时间
     deleted = models.BooleanField(verbose_name="删除", blank=True, default=False)
@@ -58,6 +62,72 @@ class BaseModel(models.Model):
         # 调用父类的save方法
         return super().save(force_insert=force_insert, force_update=force_update, using=using,
                             update_fields=update_fields)
+
+    def do_delete_update_field(self):
+        """
+        当对象删除的时候，需要自动修改的字段，加入 _del_的标识
+        """
+        if isinstance(self.delete_update_fields, (tuple, list)):
+            for field in self.delete_update_fields:
+                value = getattr(self, field)
+                if value and isinstance(value, str) and value.find('_del_') < 0:
+                    value_new = "{}_del_{}".format(value, self.strftime())
+                    setattr(self, field, value_new)
+                    try:
+                        self.save(update_fields=(field,))
+                    except Exception as e:
+                        # 比如长度超过了，就会报错
+                        print('自动修改字段报错：{}'.format(str(e)))
+                    # print("需要修改的字段：{}，值为:{}".format(field, value))
+
+    def do_delete_action(self):
+        """
+        重要系统，数据尽量只标记删除，不要做物理删除
+        当我们删除对象的时候，可能需要执行额外的任务
+        比如：修改某个字段(把某个字段加个时间戳)、删除额外的关联数据、比如关联了我的数据也需要删除掉
+        """
+        if self.deleted:
+            # 1. 判断是否已经删除，如果已经是标记删除的了，那我们就直接返回
+            return
+        else:
+            # 2. 遍历需要执行的删除的任务，我们对其进行删除
+            if isinstance(self.delete_tasks, (list, tuple)):
+                for i in self.delete_tasks:
+                    # 别循环调用自己了
+                    if i and hasattr(self, i) and i != 'do_delete_action':
+                        task_func = getattr(self, i)
+                        # 判断一下这个是否是函数
+                        if hasattr(task_func, '__call__'):
+                            # 我们执行调用函数
+                            task_func()
+                        else:
+                            print('{}不是可调用的函数', i)
+            # 3. 修改删除的字段：在delete_update_fields中会调用save方法
+            if self.delete_update_fields:
+                self.do_delete_update_field()
+
+            # 4. 修改deleted字段
+            self.deleted = True
+            update_fields = ['deleted']
+
+            # 5. 修改删除时间
+            if self.delete_time_field_name:
+                try:
+                    # 判断是否有这个字段, 如果没这个字段是会报错的
+                    self._meta.get_field(self.delete_time_field_name)
+                    setattr(self, self.delete_time_field_name, timezone.datetime.now(tz=timezone.utc))
+                    update_fields.append(self.delete_time_field_name)
+                except Exception as e:
+                    pass
+            self.save(update_fields=update_fields)
+
+    def delete(self, using=None, keep_parents=False):
+        # 1. 判断是否有do_delete_action的属性，且是可调用的
+        if hasattr(self, "do_delete_action") and callable(self.do_delete_action):
+            self.do_delete_action()
+        else:
+            # 如果没有就调用父类的删除方法，注意这里是调用models.Model的delete方法
+            super().delete(using=using, keep_parents=keep_parents)
 
     class Meta:
         # 抽象类
