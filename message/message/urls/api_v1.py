bfa2672a8577f733dbc69d316836f5e995a5cfaf
codelieche <codelieche@gmail.com>
2022-09-24 19:53:41 +0800

    remove message project

--- message/message/urls/api_v1.py
+++
@@ -1,8 +0,0 @@
-# -*- coding:utf-8 -*-
-from django.urls import path, include
-
-
-urlpatterns = [
-    # 前缀：/api/v1/
-    path("account/", include(arg=("account.urls.api", "account"), namespace="account")),
-]
